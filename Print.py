456b8b4a94cf22bca5083ba07db97e9f8acb460e
AstrocoderKS <astroks26@gmail.com>
2026-06-16 19:33:32 +1000

    1

---
+++ Print.py
@@ -0,0 +1,8 @@
+print("Welcome to the extensive facts about astronomy🪐!")
+print(5)
+print(5,"Welcome to the extensive facts about astronomy🪐!")
+print("Did you know that the solar system was formed from an explosion called the big bang occured?\n")
+print("When Earth was formed, it was a gigantic scorching ball of lava and fire.", end = "   ")
+print("Did you know a dwarf planet candidate called Sedna was close to the Sun when Earth was in its ice age?")
+
+#Overall, I think I have an extensive knowledge of astronomy for my age, and they it will slowly climb as I get older, and learn a lot more about my interests and hobbies.
